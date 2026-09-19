f7228a5ad83e23b1df7e1dea43fe4299e29dfd74
haimhm <haimhm@gmail.com>
2026-09-19 22:49:01 +0300

    Add status function

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def version():
     return "1.0"
 
 
+def status():
+    return "ok"
+
+
 if __name__ == "__main__":
     print(greet("world"))
     print(farewell("world"))
     print(version())
+    print(status())
